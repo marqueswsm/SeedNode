557119e03dae5583a9076f8b8a17feb3ec9b570f
Wagner Marques <wagnermarques@Wagners-MacBook-Pro.local>
2020-04-18 20:29:13 -0300

    created the reference migration

---
+++ migrations/20200418201308_create_table_reference.js
@@ -0,0 +1,15 @@
+const up = async (knex) => {
+  await knex.schema.createTable('referece', (table) => {
+    table.string('id').primary().notNullable();
+    table.string('description', 255);
+    table.string('citation', 255).notNullable();
+    table.string('year', 4);
+    table.string('bibtex', 255).notNullable();
+  });
+};
+
+const down = async (knex) => {
+  await knex.schema.dropTableIfExists('reference');
+};
+
+module.exports = { up, down };
