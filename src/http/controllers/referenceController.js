7a3c2c7599bb32d86d90718890ff38d549a0fe2e
Wagner Marques <wagnermarques@Wagners-MacBook-Pro.local>
2020-04-19 22:36:27 -0300

    create a constructor in the reference controller

--- src/http/controllers/referenceController.js
+++ src/http/controllers/referenceController.js
@@ -1,11 +1,12 @@
-/* eslint-disable class-methods-use-this */
-const { referenceService } = require('../../container');
-
 class ReferenceController {
+  constructor(container) {
+    this.referenceService = container.referenceService;
+  }
+
   async create(req, res) {
-    const response = await referenceService.create(req.body);
+    const response = await this.referenceService.create(req.body);
     res.send(response);
   }
 }
 
-module.exports = new ReferenceController();
+module.exports = ReferenceController;
